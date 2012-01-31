6431e99b610ef109500930719ca59a15f6c43d4b
Tim Chiu <timchiu@gmail.com>
2012-01-30 22:19:12 -0800

    added mysql settings and initial bootstrap script

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -15,6 +15,24 @@ class BootStrap {
 	def roleService
 	
     def init = { servletContext ->
+
+			def adminRole = Role.findByName("admin") ?: new Role(name: "admin", permissions: ["*:*:*"]).save(failOnError: true)
+			def memberRole = Role.findByName("member") ?: new Role(name: "member", permissions: ["taomember:*:*", "contactrecord:saveFromForm:*"]).save(failOnError: true)
+			def groupLeaderRole = Role.findByName("groupLeader") ?: new Role(name: "groupLeader", permissions: ["taomember:*:*", "contactgroup:*:*", "contactrecord:saveFromForm:*"]).save(failOnError: true)
+			def regionLeaderRole = Role.findByName("regionLeader") ?: new Role(name: "regionLeader", permissions: ["taomember:*:*", "contactgroup:*:*", "taoregion:*:*", "contactrecord:saveFromForm:*"]).save(failOnError: true)
+			def seniorLecturerRole = Role.findByName("seniorLecturer") ?: new Role(name: "seniorLecturer", permissions: ["taomember:*:*", "contactgroup:*:*", "taoregion:*:*", "contactrecord:saveFromForm:*"]).save(failOnError: true)
+			def masterRole = Role.findByName("master") ?: new Role(name: "master", permissions: ["taomember:*:*", "contactgroup:*:*", "taoregion:*:*", "contactrecord:saveFromForm:*"]).save(failOnError: true)
+
+
+			// calculate age
+			def users = TaoMember.findAllByBirthDateIsNotNullAndAgeIsNull();
+			Calendar now = Calendar.getInstance()
+			users.each {
+				it.age = (now.getTime().getTime() - it.birthDate.getTime()) / (1000 * 3600 * 24 * 365.25);
+				it.save(failOnError: true)
+			}
+
+		
     }
     def destroy = {
     }
--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -4,6 +4,7 @@ dataSource {
     username = "sa"
     password = ""
 	logSql=false
+	
 }
 hibernate {
     cache.use_second_level_cache = true
@@ -11,26 +12,37 @@ hibernate {
     cache.provider_class = 'net.sf.ehcache.hibernate.EhCacheProvider'
 }
 
-
-
 // environment specific settings
 environments {
     development {
         dataSource {
             dbCreate = "update" // one of 'create', 'create-drop','update'
-            url = "jdbc:hsqldb:mem:devDB"
+//            url = "jdbc:hsqldb:mem:devDB"
+			driverClassName = "com.mysql.jdbc.Driver"
+			url = "jdbc:mysql://localhost/fycd_contact?useUnicode=yes&characterEncoding=UTF-8"
+			username = "fillIn"
+			password = "fillIn"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:mem:testDb"
+//            url = "jdbc:hsqldb:mem:testDb"
+			dbCreate = "update" // one of 'create', 'create-drop','update'
+			driverClassName = "com.mysql.jdbc.Driver"
+			url = "jdbc:mysql://localhost/fycd_contact?useUnicode=yes&characterEncoding=UTF-8"
+			username = "fillIn"
+			password = "fillIn"
         }
     }
     production {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+//            url = "jdbc:hsqldb:file:prodDb;shutdown=true"
+			driverClassName = "com.mysql.jdbc.Driver"
+			url = "jdbc:mysql://localhost/fycd_contact?useUnicode=yes&characterEncoding=UTF-8"
+			username = "fillIn"
+			password = "fillIn"
         }
     }
 }
